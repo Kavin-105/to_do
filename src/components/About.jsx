6e9bd371420028d856de991e4db3571b71fdd4b1
Kavin-105 <kavin519088@gmail.com>
2025-12-26 21:26:01 +0530

    errror to

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 function About() {
     return(
+        <>
         <h1>About Page for the to_do</h1>
-        // <h1>About Page for the to-do</h1>
+        <h1>About Page for the to-do</h1>
+        </>
     );
 }
 export default About;
