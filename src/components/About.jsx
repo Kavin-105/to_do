6c2479387e42a73d5deb9a56db8a9b590cd04ce9
Kavin-105 <kavin519088@gmail.com>
2025-12-26 21:22:51 +0530

    error resolve

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 function About() {
     return(
         <h1>About Page for the to_do</h1>
-        <h1>About Page for the to-do</h1>
+        // <h1>About Page for the to-do</h1>
     );
 }
 export default About;
